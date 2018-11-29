b7798ed715de8e3842e3608f4ae41c0069df56fa
Kamran Syed <lovelykamran@gmail.com>
2018-11-29 11:58:53 +0500

    Updated with relevant values

--- __manifest__.py
+++ __manifest__.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
+# See LICENSE and COPYRIGHT files for full copyright and licensing details.
 {
     'name': "ksshiftmaster",
-
+	'version': '0.1',
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        Helps manage duty shifts in Hospitals, Factories, Hotels and Ships etc.""",
 
     'description': """
-        Long description of module's purpose
+        Helps manage duty shifts in Hospitals, Factories, Hotels and Ships etc.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'author': "Kamran Syed",
+    'website': "http://agilesolutionspk.com",
+    'license': "GPL-3",
+	'installable': True,
+    'application': True,
+	'auto_install': True,
+    'category': 'Human Resources',
+    'depends': ['base', 'hr'],
 
     # always loaded
     'data': [
